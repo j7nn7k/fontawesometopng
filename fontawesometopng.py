05a55cb72119970863d91fd9a0999bcdb0705fbe
JNK <jannik.weyrich@particulate.me>
2014-05-24 15:51:17 +0200

    added generate method to test the frontend

--- fontawesometopng.py
+++ fontawesometopng.py
@@ -15,13 +15,47 @@ app.config.update(dict(
 app.config.from_envvar('FATP_SETTINGS', silent=True)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        # send data to transform script
-        pass
-    return render_template('home.html', error=error)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/generate')
+def generate():
+    icon_name = validate_name(request.args.get('name'))
+    icon_color = validate_color(request.args.get('color'))
+    icon_size = validate_size(request.args.get('size'))
+
+    # icon_url = generate(icon_name, icon_color, icon_size)
+
+    icon_url = icon_name + icon_color + icon_size
+
+    return icon_url
+
+
+def validate_name(name):
+    if name:
+        # TODO add proper error handling and regex
+        return name.lower().replace('fa-', '').replace(' ', '').replace('-', '').replace('_', '').replace('#', '')
+    else:
+        raise ValueError("Please provide an icon name!")
+
+
+def validate_color(color):
+    if color:
+        # TODO add proper error handling and regex
+        return color.lower().replace(' ', '').replace('-', '').replace('_', '').replace('#', '')
+    else:
+        return '000'
+
+
+def validate_size(size):
+    if size:
+        # TODO add proper error handling and regex
+        return size.lower().replace(' ', '').replace('-', '').replace('_', '').replace('#', '')
+    else:
+        return '32'
+
 
 if __name__ == '__main__':
     app.run()
