5915a76e062c66a23988ae490049e4cde444c09c
Jannik Weyrich <jannikweyrich@gmail.com>
2014-05-27 17:33:42 +0200

    updated rate limit tester

--- nginx_rate_limit_tester.py
+++ nginx_rate_limit_tester.py
@@ -4,4 +4,4 @@ import time
 start_time = time.time()
 for x in range(100):
     print time.time() - start_time, "seconds"
-    print urllib2.urlopen("http://fa2png.io/generate?name=play&size=32%s&color=000000").read()
+    print urllib2.urlopen("http://fa2png.io/generate?name=paper-plane&size=10&color=HASH9e96cf").read()
